1e249fb0aa3f20af04728c43fcb0b11774772c27
Gotzon Alava <gotzonalava@icloud.com>
2024-02-23 13:03:54 +0100

    Various updates

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ import { WebsiteRoutes } from './server/routes/Website.routes.js';
 import { PartialsRoutes } from './server/routes/Partials.routes.js';
 // import { APIRoutes } from './server/routes/Api.routes.js';
 
-dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const envPath = path.join(__dirname, `${process.env.NODE_ENV}.env`);
+dotenv.config({ path: envPath });
 
 const fastify = Fastify({ logger: true, ignoreTrailingSlash: true });
 const port = process.env.PORT || 3000;
