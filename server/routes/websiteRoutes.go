ae5380a54bb383d115de68c4e880a7ae9f4c2f51
Gotzon Alava <gotzonalava@icloud.com>
2024-02-27 16:01:00 +0100

    Removed any code and references for i18n, we'll cross that bridge at some point

--- server/routes/websiteRoutes.go
+++ server/routes/websiteRoutes.go
@@ -1,39 +1,13 @@
 package routes
 
 import (
-	"encoding/json"
 	"html/template"
-	"io/ioutil"
 	"net/http"
 	"path/filepath"
-	"strings"
 )
 
 func HandleWebsiteRequest(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
-
-	if strings.HasSuffix(path, "/") {
-		if strings.HasPrefix(path, "/es/") {
-			path = path + "inicio"
-		} else if strings.HasPrefix(path, "/en/") {
-			path = path + "index"
-		} else {
-			path = "/es" + path + "inicio"
-		}
-	}
-
-	file, err := ioutil.ReadFile("data" + path + ".json")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	var data map[string]interface{}
-	err = json.Unmarshal(file, &data)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 
 	tmpl, err := template.ParseFiles(filepath.Join("pages", path+".html"))
 	if err != nil {
@@ -41,7 +15,7 @@ func HandleWebsiteRequest(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = tmpl.Execute(w, data)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
