ca5db6f7027009a290b285f7cbff7ed27e5bea5d
Gotzon Alava <gotzonalava@icloud.com>
2024-02-27 12:44:52 +0100

    Reorgarnized a bit the website routes

---
+++ server/routes/websiteRoutes.go
@@ -0,0 +1,48 @@
+package websiteRoutes
+
+import (
+    "encoding/json"
+    "html/template"
+    "io/ioutil"
+    "net/http"
+    "path/filepath"
+    "strings"
+)
+
+func HandleRequest(w http.ResponseWriter, r *http.Request) {
+    path := r.URL.Path
+
+    if strings.HasSuffix(path, "/") {
+        if strings.HasPrefix(path, "/es/") {
+            path = path + "inicio"
+        } else if strings.HasPrefix(path, "/en/") {
+            path = path + "index"
+        } else {
+            path = "/es" + path + "inicio"
+        }
+    }
+
+    file, err := ioutil.ReadFile("data" + path + ".json")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    var data map[string]interface{}
+    err = json.Unmarshal(file, &data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    tmpl, err := template.ParseFiles(filepath.Join("pages", path+".html"))
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    err = tmpl.Execute(w, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
--- start.go
+++ start.go
@@ -1,49 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"html/template"
-	"io/ioutil"
 	"net/http"
-	"path/filepath"
 	"gtz-main/server/middleware"
-	"strings"
+	"gtz-main/server/routes"
 )
 
 func main() {
 
     http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("dist/assets"))))
 
-	http.Handle("/", middleware.LoggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		if strings.HasSuffix(path, "/") {
-			path = path + "index"
-		}
-
-		file, err := ioutil.ReadFile("data" + path + ".json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		var data map[string]interface{}
-		err = json.Unmarshal(file, &data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		tmpl, err := template.ParseFiles(filepath.Join("pages", path + ".html"))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.Execute(w, data)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})))
+	http.Handle("/", middleware.LoggingMiddleware(http.HandlerFunc(websiteRoutes.HandleRequest)))
 
 	http.ListenAndServe(":3000", nil)
 }
