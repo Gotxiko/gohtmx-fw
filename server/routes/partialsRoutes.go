caa13e67eaed6049b2d1a94d5f2606e8b81c42a2
Gotzon Alava <gotzonalava@icloud.com>
2024-02-27 21:14:31 +0100

    Bugfix?

--- server/routes/partialsRoutes.go
+++ server/routes/partialsRoutes.go
@@ -4,7 +4,6 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
-	"strings"
 )
 
 func HandlePartialRequest(w http.ResponseWriter, r *http.Request) {
@@ -14,9 +13,6 @@ func HandlePartialRequest(w http.ResponseWriter, r *http.Request) {
 	}
 
 	path := r.URL.Path
-	if strings.HasPrefix(path, "/") {
-		path = strings.TrimPrefix(path, "/")
-	}
 
 	tmpl, err := template.ParseFiles(filepath.Join(path + ".html"))
 	if err != nil {
