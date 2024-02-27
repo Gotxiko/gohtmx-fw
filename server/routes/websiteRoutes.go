67e3b7bc72ae6a0388127a507449f4edadc21a64
Gotzon Alava <gotzonalava@icloud.com>
2024-02-27 13:19:06 +0100

    Formatting & package.json cleanup form unused libraries

--- server/routes/websiteRoutes.go
+++ server/routes/websiteRoutes.go
@@ -1,48 +1,48 @@
 package routes
 
 import (
-    "encoding/json"
-    "html/template"
-    "io/ioutil"
-    "net/http"
-    "path/filepath"
-    "strings"
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
 )
 
 func HandleWebsiteRequest(w http.ResponseWriter, r *http.Request) {
-    path := r.URL.Path
+	path := r.URL.Path
 
-    if strings.HasSuffix(path, "/") {
-        if strings.HasPrefix(path, "/es/") {
-            path = path + "inicio"
-        } else if strings.HasPrefix(path, "/en/") {
-            path = path + "index"
-        } else {
-            path = "/es" + path + "inicio"
-        }
-    }
+	if strings.HasSuffix(path, "/") {
+		if strings.HasPrefix(path, "/es/") {
+			path = path + "inicio"
+		} else if strings.HasPrefix(path, "/en/") {
+			path = path + "index"
+		} else {
+			path = "/es" + path + "inicio"
+		}
+	}
 
-    file, err := ioutil.ReadFile("data" + path + ".json")
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	file, err := ioutil.ReadFile("data" + path + ".json")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    var data map[string]interface{}
-    err = json.Unmarshal(file, &data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	var data map[string]interface{}
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    tmpl, err := template.ParseFiles(filepath.Join("pages", path+".html"))
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	tmpl, err := template.ParseFiles(filepath.Join("pages", path+".html"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-    err = tmpl.Execute(w, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
--- start.go
+++ start.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net/http"
 	"gtz-main/server/middleware"
 	"gtz-main/server/routes"
+	"net/http"
 )
 
 func main() {
 
-    http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("dist/assets"))))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("dist/assets"))))
 
-   	http.Handle("/partials/", middleware.LoggingMiddleware(http.HandlerFunc(routes.HandlePartialRequest)))
+	http.Handle("/partials/", middleware.LoggingMiddleware(http.HandlerFunc(routes.HandlePartialRequest)))
 	http.Handle("/", middleware.LoggingMiddleware(http.HandlerFunc(routes.HandleWebsiteRequest)))
 
 	http.ListenAndServe(":3000", nil)
