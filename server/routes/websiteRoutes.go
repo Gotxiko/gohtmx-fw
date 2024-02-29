98b6f8d4224d4a9f7d1f3abd05fb1989846e7209
Gotzon Alava <gotzonalava@icloud.com>
2024-02-29 09:54:46 +0100

    More updates

--- server/routes/websiteRoutes.go
+++ server/routes/websiteRoutes.go
@@ -18,20 +18,50 @@ type Langs map[string]interface{}
 var langs map[string]Langs
 
 func init() {
-    langs = make(map[string]Langs)
-    for _, lang := range []string{"en", "es"} {
-        langData, err := loadLangs(lang)
-        if err != nil {
-            panic(err)
-        }
-        langs[lang] = langData
-    }
+	langs = make(map[string]Langs)
+	for _, lang := range []string{"en", "es"} {
+		langData, err := loadLangs(lang)
+		if err != nil {
+			panic(err)
+		}
+		langs[lang] = langData
+	}
 }
 
 func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	lang := vars["lang"]
-	slug := vars["slug"]
+	lang, langExists := vars["lang"]
+	slug, slugExists := vars["slug"]
+
+	// Handle the root route "/"
+	if r.URL.Path == "/" {
+		http.Redirect(w, r, "/es/inicio", http.StatusMovedPermanently)
+		return
+	}
+
+	// Handle the "/es/" and "/en/" routes
+	if !langExists || (langExists && slug == "") {
+		switch lang {
+		case "es":
+			http.Redirect(w, r, "/es/inicio", http.StatusMovedPermanently)
+		case "en":
+			http.Redirect(w, r, "/en/home", http.StatusMovedPermanently)
+		default:
+			http.Redirect(w, r, "/es/inicio", http.StatusMovedPermanently)
+		}
+		return
+	}
+
+	// Existing code for handling "/:lang/:slug" routes
+	if !slugExists {
+		switch lang {
+		case "es":
+			http.Redirect(w, r, "/es/inicio", http.StatusMovedPermanently)
+		case "en":
+			http.Redirect(w, r, "/en/home", http.StatusMovedPermanently)
+		}
+	}
+
 	w.WriteHeader(http.StatusOK)
 
 	filename, err := getFilenameFromSlug(slug)
@@ -40,7 +70,7 @@ func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tmpl, err := template.ParseFiles(filepath.Join("pages", filename + ".html"))
+	tmpl, err := template.ParseFiles(filepath.Join("pages", filename+".html"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -59,37 +89,38 @@ func WebsiteHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func loadLangs(lang string) (Langs, error) {
-    jsonFile, err := os.Open(fmt.Sprintf("locales/%s/langs.json", lang))
-    if err != nil {
-        return nil, err
-    }
-    defer jsonFile.Close()
+	jsonFile, err := os.Open(fmt.Sprintf("locales/%s/langs.json", lang))
+	if err != nil {
+		return nil, err
+	}
+	defer jsonFile.Close()
 
-    byteValue, err := io.ReadAll(jsonFile)
-    if err != nil {
-        return nil, err
-    }
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
 
-    var langs Langs
-    json.Unmarshal(byteValue, &langs)
+	var langs Langs
 
-    return langs, nil
+	json.Unmarshal(byteValue, &langs)
+
+	return langs, nil
 }
 
 func getFilenameFromSlug(slug string) (string, error) {
-    jsonFile, err := os.Open("locales/slugToFileMap.json")
-    if err != nil {
-        return "", err
-    }
-    defer jsonFile.Close()
+	jsonFile, err := os.Open("locales/slugToFileMap.json")
+	if err != nil {
+		return "", err
+	}
+	defer jsonFile.Close()
 
-    byteValue, err := io.ReadAll(jsonFile)
-    if err != nil {
-        return "", err
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return "", err
 	}
 
-    var slugMap SlugMap
-    json.Unmarshal(byteValue, &slugMap)
+	var slugMap SlugMap
+	json.Unmarshal(byteValue, &slugMap)
 
-    return slugMap[slug], nil
+	return slugMap[slug], nil
 }
--- start.go
+++ start.go
@@ -12,7 +12,6 @@ func main() {
 
 	r := mux.NewRouter()
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("dist/assets"))))
-	// r.HandleFunc("/", Homehandler)
 	r.HandleFunc("/{lang}/{slug}", routes.WebsiteHandler)
 
 	http.ListenAndServe(":3000", r)
