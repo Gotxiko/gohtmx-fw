2c0b3f68bdb36c168d0f83c903a84f66de88969b
Gotzon Alava <gotzonalava@icloud.com>
2024-02-29 15:22:46 +0100

    Updates and stuff

--- server/routes/partialsRoutes.go
+++ server/routes/partialsRoutes.go
@@ -16,13 +16,22 @@ func PartialsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Load langs file from the request header language to send the proper language contents to the partial
+	lang := r.Header.Get("Accept-Language")
+
+	langData, ok := langs[lang][name].(map[string]interface{})
+	if !ok {
+		http.Error(w, "Language or slug not found", http.StatusNotFound)
+		return
+	}
+
 	tmpl, err := template.ParseFiles(filepath.Join("partials", name+".html"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	err = tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, langData)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
