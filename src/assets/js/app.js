99cacd8f64daf0fada89b22e02e280004e81712b
Gotzon Alava <gotzonalava@icloud.com>
2024-03-07 13:15:23 +0100

    Moved styles and scripts to its files

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,3 +6,16 @@ import * as gtm from './Analytics/init.js';
 
 // Cookie Consent
 import * as CookieConsent from './CookieConsent/init.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('button').forEach((button) => {
+        button.addEventListener('click', (e) => {
+            document.querySelectorAll('button').forEach((button) => {
+                button.querySelector('span:first-child').classList.remove('bg-black');
+            });
+            let targetElement =
+                e.target.tagName.toLowerCase() === 'button' ? e.target : e.target.parentElement;
+            targetElement.querySelector('span:first-child').classList.add('bg-black');
+        });
+    });
+});
